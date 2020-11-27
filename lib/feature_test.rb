57fabbd13310f9bc5d65549e7b87af26081aee72
ollienorman <oliver.norman1991@gmail.com>
2020-11-27 13:08:29 +0000

    added failing birthdays_today test

--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -1,7 +1,11 @@
 require './birthday_list.rb'
 
+puts "Name?"
+name = gets.chomp
+puts "Birthday?"
+birthday = gets.chomp
 add_birthday(name, birthday)
 
 print_birthdays
 
-birthdays_today?
+birthdays_today
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -4,10 +4,16 @@ describe BirthdayList do
   describe "#add_birthday" do
     it "adds a birthday that can be accessed later" do
       birthday_list = BirthdayList.new
-      birthday = "06-06-1991"
-      name = "Ollie"
+      birthday = "27-11-1991"
+      name = "Test"
       birthday_list.add_birthday(name, birthday)
-      expect(birthday_list.print_birthdays).to eq "Name: Ollie, Birthday: 06-06-91"
+      expect(birthday_list.print_birthdays).to eq "Name: Test, Birthday: 27-11-91"
+    end
+  end
+
+  describe "#birthdays_today" do
+    it "checks to see if any birthdays are today, and returns the result" do
+      expect(birthday_list.birthdays.today).to eq "Name: Test"
     end
   end
 end
