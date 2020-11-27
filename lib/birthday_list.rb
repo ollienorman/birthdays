a8abeff09b52fb26dc11eaa6c8daa7c03411b91f
ollienorman <oliver.norman1991@gmail.com>
2020-11-27 12:29:29 +0000

    added add_birthday functionality that passes first rspec test

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,9 +1,16 @@
 class BirthdayList
   
+  def initialize
+    @birthday_hash = {}
+  end
+
   def add_birthday(name, birthday)
+    @birthday_hash[name.to_sym] = birthday
   end
 
   def print_birthdays
+    @birthday_hash.each { |name, birthday| 
+    return "Name: #{name.to_s}, Birthday: #{birthday.strftime("%d-%m-%y")}" }
   end
-  
+
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,7 +5,8 @@ describe BirthdayList do
     it "adds a birthday that can be accessed later" do
       birthday_list = BirthdayList.new
       birthday = Time.new(1991, 6, 6)
-      birthday_list.add_birthday("Ollie", birthday)
+      name = "Ollie"
+      birthday_list.add_birthday(name, birthday)
       expect(birthday_list.print_birthdays).to eq "Name: Ollie, Birthday: 06-06-91"
     end
   end
