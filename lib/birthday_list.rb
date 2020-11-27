12c17b912e65c2dc835d0bf4c7ca57fd8022548f
ollienorman <oliver.norman1991@gmail.com>
2020-11-27 12:03:03 +0000

    added methods

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,9 @@
+class BirthdayList
+  
+  def add_birthday(name, birthday)
+  end
 
+  def print_birthdays
+  end
+  
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,7 +5,7 @@ describe BirthdayList do
     it "adds a birthday that can be accessed later" do
       birthday_list = BirthdayList.new
       birthday = Time.new(1991, 6, 6)
-      add_birthday("Ollie", birthday)
+      birthday_list.add_birthday("Ollie", birthday)
       expect(birthday_list.print_birthdays).to eq "Name: Ollie, Birthday: 06-06-91"
     end
   end
