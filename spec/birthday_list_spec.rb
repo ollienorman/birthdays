892289902b102d03c1bdf2336365b65e83181594
ollienorman <oliver.norman1991@gmail.com>
2020-11-27 11:57:39 +0000

    added first unit test - trying to test behaviour, not state

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,15 @@
+require 'birthday_list'
 
+describe BirthdayList do
+
+    describe "#add_birthday" do
+        it "adds a birthday that can be accessed later" do
+            birthday_list = BirthdayList.new
+            birthday = Time.new(1991, 6, 6)
+            add_birthday("Ollie", birthday)
+            expect(birthday_list.print_birthdays).to eq "Name: Ollie, Birthday: 06-06-91"
+        end
+    end
+
+
+end
